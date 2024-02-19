9e284f5637d03aee84dac3f66ea4f48c77f6c781
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-02-19 14:02:57 +0300

    add all file

---
+++ src/main/java/com/poja/poja_sary_std22108/file/FileHash.java
@@ -0,0 +1,6 @@
+package com.poja.poja_sary_std22108.file;
+
+import com.poja.poja_sary_std22108.PojaGenerated;
+
+@PojaGenerated
+public record FileHash(FileHashAlgorithm algorithm, String value) {}
---
+++ src/main/java/com/poja/poja_sary_std22108/repository/model/Dummy.java
@@ -0,0 +1,15 @@
+package com.poja.poja_sary_std22108.repository.model;
+
+import com.poja.poja_sary_std22108.PojaGenerated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@PojaGenerated
+@Entity
+@Getter
+@Setter
+public class Dummy {
+  @Id private String id;
+}
